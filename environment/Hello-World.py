9ae1e134ac563ba5bd838f9d736f008cd4f5d43e
GanvirAishwarya <129280682+GanvirAishwarya@users.noreply.github.com>
2023-04-03 23:43:06 +0530

    Add files via upload

---
+++ environment/Hello-World.py
@@ -0,0 +1,9 @@
+#   print("Hello, World")
+
+############ lab 109 ####################
+
+print("Python has three numeric types: int, float, and complex")
+
+
+
+ 
---
+++ environment/lab-python-file-name.py
@@ -0,0 +1,47 @@
+############## lab109 ##################
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+# Introducing the float data type #
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+# Introducing the complex data type #
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+# Introducing the bool data type #
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+# bool data type # 
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+#
+#
+#########lab 110  Introducing the string data type ############
+#
+myString = "This is a string."
+print(myString)
+print(type(myString))
+print(myString + " is of the data type " + str(type(myString)))
+# Working with string concatenation #
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+# Working with input strings #
+name = input("What is your name? ")
+print(name)
+# Formatting output strings #
+color = input("What is your favorite color?  ")
+animal = input("What is your favorite animal?  ")
+print("{}, you like a {} {}!".format(name,color,animal))
+
+
